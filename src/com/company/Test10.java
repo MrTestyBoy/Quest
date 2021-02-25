c84c07201ad714e6c99ac8c81131bf3505d88daf
OoOoO <Goomanyq@gmail.com>
2021-02-26 00:40:56 +0500

    Edit 1,6 Add 10

--- src/com/company/Test10.java
+++ src/com/company/Test10.java
@@ -1,7 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Test10 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("array size x:");
+        int x = in.nextInt();
+        System.out.print("array size y:");
+        int y = in.nextInt();
+        int z;
+        int[][] Ar = new int[x][y];
+        for(int i = 0; i<x; i++ ){
+            for(int j = 0; j<y; j++){
+                System.out.print(" Value Ar["+i+"]["+j+"]");
+                z = in.nextInt();
+                Ar[i][j] = z;
+            }
+            System.out.println();
+        }
+        System.out.print("Result: ");
+        for(int i=0;i<x;i++){
+            System.out.printf("%d ", Ar[i][0]*3);
+        }
 
     }
 }
--- src/com/company/Test6.java
+++ src/com/company/Test6.java
@@ -6,13 +6,22 @@ import java.util.Scanner;
 public class Test6 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int Res = 0;
         System.out.print("1 - weight, 2 - distance:");
         int I1 = in.nextInt();
-        System.out.print("value:");
-        int I2 = in.nextInt();
-        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+
+
         if(I1 == 1){
+            System.out.print("1 - Грамм, 2 - Унция, 3 - Фунт, 4 - Гран:");
+            int I3 = in.nextInt();
+            System.out.print("value:");
+            double I2 = in.nextDouble();
+            switch(I3){
+                case  (1):break;
+                case (2):I2/=0.035274;break;
+                case (3):I2/=0.00220462;break;
+                case (4):I2/=15.4324;break;
+            }
+            DecimalFormat decimalFormat = new DecimalFormat("#.##");
             System.out.println("Грамм: " + I2);
             String result = decimalFormat.format(I2*0.035274);
             System.out.println("Унция: " + result);
@@ -22,6 +31,17 @@ public class Test6 {
             System.out.println("Гран: " + result);
         }
         if(I1 == 2){
+            System.out.print("1 - Метры, 2 - Мили, 3 - Ярды, 4 - Футы:");
+            int I3 = in.nextInt();
+            System.out.print("value:");
+            double I2 = in.nextDouble();
+            switch(I3){
+                case  (1):break;
+                case (2):I2/=0.0006;break;
+                case (3):I2/=1.09361;break;
+                case (4):I2/=3.28084;break;
+            }
+            DecimalFormat decimalFormat = new DecimalFormat("#.##");
             System.out.println("Метры: " + I2);
             String result = decimalFormat.format(I2*0.0006);
             System.out.println("Мили: " + result);
